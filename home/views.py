a3d101d41cca00a465fb9324d2ae6c267dd00bff
spencer-boverman <54991979+spencer-boverman@users.noreply.github.com>
2019-11-19 18:38:53 -0700

    merged

--- home/views.py
+++ home/views.py
@@ -1,9 +1,19 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.http import HttpResponse
+from users.models import User
+from schedules.models import Schedule
+from classes.models import Class
 
 # Create your views here.
 
 
 def home(request):
+    if request.user.is_authenticated:
+        class_list = request.user.schedule.classes.all()
+        context = {'class_list':class_list}
+        return ("Hello world", "home/home.html", context)
+
     return render(request, "home/home.html")
+
 
