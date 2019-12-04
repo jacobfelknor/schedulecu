1db3bcaa06e1e664a3ac15942c093a3a0cd7583d
PaperXLV <samuel.j.williams@colorado.edu>
2019-12-04 14:28:36 -0700

    fixing travis

--- config/settings/release.py
+++ config/settings/release.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['secret_key']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com"]
 
 
 # Application definition
