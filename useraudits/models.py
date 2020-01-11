5c1d3cd107a74c200b05eb100ae85d7ce7f5b69e
WhyHelloPear <kristian.s.montoya@gmail.com>
2020-01-10 17:42:19 -0700

    Format fix

--- useraudits/models.py
+++ useraudits/models.py
@@ -6,8 +6,8 @@ from classes.models import Class
 
 # Model to define the classes a user has completed. Essentially a copy of schedule
 class UserAuditEntry(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="useraudits")
+	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="useraudits")
 	course = models.ForeignKey(Class, on_delete=models.CASCADE, related_name="useraudits")
-    year = models.CharField(max_length=10)
-    semester = models.CharField(max_length=10)
-    grade = models.CharField(max_length=10)
+	year = models.CharField(max_length=10)
+	semester = models.CharField(max_length=10)
+	grade = models.CharField(max_length=10)
